50e9645c32c7e2ae0709e11a284b025b3c7b0af2
Ewan <Ewan@192.168.0.22>
2017-11-25 09:36:25 -0500

    Volatile Image + Perfect Pixel locating

--- ImageFormation/src/main/ImageFormation.java
+++ ImageFormation/src/main/ImageFormation.java
@@ -2,7 +2,10 @@ package main;
 
 import java.awt.FileDialog;
 import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
 import java.awt.image.BufferedImage;
+import java.awt.image.VolatileImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
@@ -12,13 +15,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class ImageFormation extends JPanel{
+	private static final long serialVersionUID = 1L;
 	static BufferedImage i;
 	static int w = 0;
 	static int h = 0;
 	static Particle[][] particles;
 	static Random rand = new Random();
 	public static void main(String args[]){
-//		System.setProperty("sun.java2d.opengl","True");
+		System.setProperty("sun.java2d.opengl","True");
 		FileDialog fd = new FileDialog((java.awt.Frame) null);
 		fd.setTitle("Choose an image");
 		fd.setVisible(true);
@@ -47,8 +51,13 @@ public class ImageFormation extends JPanel{
 		frame.setVisible(true);
 		frame.setFocusable(true);
 		while(true){
+			long t0 = System.nanoTime();
 			m.repaint();
+			long t1 = System.nanoTime();
 			m.update();
+			long t2 = System.nanoTime();
+//			System.out.print((t1 - t0) / 1000000 + " ");
+//			System.out.println((t2 - t1) / 1000000);
 		}
 	}
 	public static double getDistance( double x1,  double y1,  double x2,  double y2) {
@@ -61,25 +70,47 @@ public class ImageFormation extends JPanel{
 				double dist = getDistance(p.x, p.y, p.targetX, p.targetY); 
 				double deltaX = (p.x - p.targetX) / dist;
 				double deltaY = (p.y - p.targetY) / dist;
-				p.vX -= deltaX / 10;
-				p.vY -= deltaY / 10;
+				p.vX -= deltaX / 30;
+				p.vY -= deltaY / 30;
+				
+				if(dist < 1){
+					double fX = Math.floor(p.x) - p.targetX;
+					double fY = Math.floor(p.x) - p.targetX;
+					if(fX == 0){
+						p.x = p.targetX;
+						p.vX = 0;
+					}
+					if(fY == 0){
+						p.y = p.targetY;
+						p.vY = 0;
+					}
+					p.vX -= p.vX / 2;
+					p.vY -= p.vY / 2;
+				}
+				else{
+					p.vX -= p.vX / 50;
+					p.vY -= p.vY / 50;
+				}
 				p.x += p.vX;
 				p.y += p.vY;
-				p.vX -= p.vX / 100;
-				p.vY -= p.vY / 100;
 			}
 		}
 	}
 	public void paint(Graphics g){
 		super.paint(g);
-//		System.out.println("hey");
+		GraphicsConfiguration gc = GraphicsEnvironment.
+				getLocalGraphicsEnvironment().getDefaultScreenDevice().
+				getDefaultConfiguration();
+		VolatileImage retVal = gc.createCompatibleVolatileImage(w, h);
+		Graphics gV = retVal.createGraphics();
 		for(int c = 0; c < w;c++){
 			for(int r = 0; r < h;r++){
 				Particle p = particles[c][r];
-				g.setColor(p.RGB);
-				g.fillRect((int)p.x, (int)p.y, 1, 1);
+				gV.setColor(p.RGB);
+				gV.fillRect((int)Math.floor(p.x), (int)Math.floor(p.y), 1, 1);
 			}
 		}
+		g.drawImage(retVal, 0, 0, null);
 	}
 
 }
